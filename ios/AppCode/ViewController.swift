6f8923029d551ab3785f1ac9baee1b5814378448
leontaolong <longt8@uw.edu>
2018-01-16 12:35:35 -0800

    Add back ios/

---
+++ ios/AppCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppCode
+//
+//  Created by LEON LOONG on 1/16/18.
+//  Copyright © 2018 leontaolong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
