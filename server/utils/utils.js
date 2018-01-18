99f275f4b33e0d5dbf031516a45ae334f4cc30de
leontaolong <longt8@uw.edu>
2018-01-17 17:13:01 -0800

    Enable account login

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -2,13 +2,21 @@
 
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
+const registerUrl = 'https://sdb.admin.uw.edu/students/uwnetid/register.asp'
+const loginTestUrl = 'https://my.uw.edu'
+
 let Utils = {
     submit: async function(courseInfo) {
         let driver = await new Builder().forBrowser('chrome').build();
         console.log("WebDriver successfully built.")
+
         try {
-            await driver.get('http://www.google.com');
-            // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+            await driver.get(loginTestUrl));
+
+            // login with credentials
+            await driver.findElement(By.id('weblogin_netid')).sendKeys(courseInfo.username);
+            await driver.findElement(By.id('weblogin_password')).sendKeys(courseInfo.password);
+            await driver.findElement(By.xpath('//*[@id="main"]/div[1]/form/ul[2]/li/input')).click();
             // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
         } finally {
             // await driver.quit();
