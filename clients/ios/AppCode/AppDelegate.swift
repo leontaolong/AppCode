a83ce83ca8ff45b57fdf30bc6c5efd33bce2faa0
leontaolong <longt8@uw.edu>
2018-02-01 03:49:42 -0800

    Change status bar color to white

--- clients/ios/AppCode/AppDelegate.swift
+++ clients/ios/AppCode/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // set status bar color to light
+        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 
