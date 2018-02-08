c28a75129b726b5953b111ab74916299c31026e1
leontaolong <longt8@uw.edu>
2018-02-07 21:10:36 -0800

    Implement local db for accountInfo

--- clients/ios/AppCode/SecondViewController.swift
+++ clients/ios/AppCode/SecondViewController.swift
@@ -13,6 +13,9 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var pwdText: UITextField!
     
+    private var accountInfo:[String:String] = [:]
+    private let db = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,7 +23,12 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         emailText.delegate = self;
         pwdText.delegate = self;
         
-        // Do any additional setup after loading the view, typically from a nib.
+        // get accountInfo from local db
+        if let dbAccountInfo = db.dictionary(forKey: "accountInfo") {
+            accountInfo = dbAccountInfo as! [String : String]
+            emailText.text = accountInfo["email"]
+            pwdText.text = accountInfo["password"]
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +37,11 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func btnUpdate(_ sender: UIButton) {
+        // set accountInfo into local db
+        accountInfo["email"] = emailText.text
+        accountInfo["password"] = pwdText.text
+        db.set(accountInfo, forKey: "accountInfo")
+        
         let alert = UIAlertController(title: "Success", message: "Account Info Updated Successfully!", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Done", style: .default))
         self.present(alert, animated: true, completion: nil)
