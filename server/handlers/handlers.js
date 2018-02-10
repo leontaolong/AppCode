31a002db711f8bf7576ecf43791a0977bac8255e
leontaolong <longt8@uw.edu>
2018-02-10 02:57:21 -0800

    Add login failed error handling in server

--- server/handlers/handlers.js
+++ server/handlers/handlers.js
@@ -12,7 +12,7 @@ module.exports = function() {
     router.post('/v1/register', (req, res, next) => {
         let infoJSON = req.body;
         if (!infoJSON || !infoJSON.username || !infoJSON.password || !infoJSON.sln1) { // handle request error
-            res.status(400).send("Bad Request Error: Course or account information missing")
+            res.status(400).send("Bad Request Error: Course or account information missing");
         } else {
             var info = new CourseInfo(infoJSON);
             console.log(info);
@@ -22,12 +22,6 @@ module.exports = function() {
                 next(err);
             }
         }
-
-        // try {
-        //     res.send("request succeesful");
-        // } catch (err) {
-        //     next(err);
-        // }
     });
 
     return router;
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -5,8 +5,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 const registerUrl = 'https://sdb.admin.uw.edu/students/uwnetid/register.asp'
 const loginTestUrl = 'https://my.uw.edu'
 
+
 let Utils = {
-    submit: async function(courseInfo, res, next) {
+    submit: async (courseInfo, res, next) => {
         let driver = await new Builder().forBrowser('chrome').build();
         console.log("WebDriver successfully built.")
 
@@ -17,7 +18,19 @@ let Utils = {
             await driver.findElement(By.id('weblogin_netid')).sendKeys(courseInfo.username);
             await driver.findElement(By.id('weblogin_password')).sendKeys(courseInfo.password);
             await driver.findElement(By.xpath('//*[@id="main"]/div[1]/form/ul[2]/li/input')).click();
-            res.send("Registration succeesful!");
+            let currentSite = await driver.getCurrentUrl();
+            if (currentSite != registerUrl) { // login failed
+                res.status(400).send("UW Login Faild: Account info is not correct.");
+            } else {
+                /*TODO:
+                - determine sln
+                - find cell, enter sln x n
+                - click submit
+                - get report from <p>
+                - response.send    
+                */
+                res.send("Registration succeesful!");
+            }        
         } finally {
             // await driver.quit();
         }
